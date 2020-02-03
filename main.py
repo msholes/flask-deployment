3212593ecda3dcd2d713e66981bbe842cebfec88
Michael Sholes <msholes06@gmail.com>
2020-02-02 19:34:39 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+APP_VERSION = 4.0
+
+@app.route('/')
+def hello_world():
+    return 'Hello, My name is Michael and I Like AI and ML!'
+
+@app.route('/json/<value>')
+def ajson(value):
+    return jsonify({"json": value})
+
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
